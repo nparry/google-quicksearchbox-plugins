419288560ce39a935fd4c54ab4734b7342fae712
Nathan Parry <nparry@gmail.com>
2009-01-25 17:24:25 -0500

    Add source code for delicious plugin

---
+++ Modules/DeliciousBookmarks/DeliciousBookmarksSource.m
@@ -0,0 +1,316 @@
+//
+//  DeliciousBookmarksSource.m
+//
+//  Copyright (c) 2009 Nathan Parry. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//    * Redistributions of source code must retain the above copyright
+//  notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above
+//  copyright notice, this list of conditions and the following disclaimer
+//  in the documentation and/or other materials provided with the
+//  distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import <Vermilion/Vermilion.h>
+#import "KeychainItem.h"
+#import "DeliciousClient.h"
+
+static NSString *const kHardcodedDeliciousUsername 
+	= @"your_username";
+
+static NSString *const kHardcodedDeliciousPassword
+	= @"your_password";
+
+static NSString *const kObjectAttributeDeliciousTags 
+	= @"ObjectAttributeDeliciousTags";
+
+@interface DeliciousBookmarksSourc : HGSMemorySearchSource <HGSAccountClientProtocol> {
+	NSTimer *updateTimer_;
+	DeliciousClient *deliciousClient_;
+	BOOL currentlyFetching_;
+	NSString *accountIdentifier_;
+	NSImage *tagIcon_;
+}
+- (void)setUpPeriodicRefresh;
+- (void)startAsynchronousBookmarkFetch;
+- (void)refreshBookmarks:(NSTimer *)timer;
+- (void)loginCredentialsChanged:(id)object;
+- (void)bookmarkUpdateFailedWithError:(NSError *)error;
+- (void)bookmarkUpdateProducedNoChange;
+- (void)bookmarkUpdateProducedNewBookmarks:(NSArray *)bookmarks forUser:(NSString *)username;
+- (HGSObject *)makeResultForUrl: (NSString *)url
+                          title:(NSString *)title
+                           type:(NSString *)type
+                           tags:(NSSet *)tags
+                           icon:(NSImage*)iconImage;
+@end
+
+@implementation DeliciousBookmarksSource
+
+- (id)initWithConfiguration:(NSDictionary *)configuration {
+	if ((self = [super initWithConfiguration:configuration])) {
+		NSBundle* sourceBundle = HGSGetPluginBundle();
+		NSString *iconPath = [sourceBundle pathForImageResource:@"delicious"];
+		tagIcon_ = [[NSImage alloc] initByReferencingFile:iconPath];
+		
+		[self startAsynchronousBookmarkFetch];
+		[self setUpPeriodicRefresh];
+		
+		/*
+		id<HGSAccount> account
+			= [configuration objectForKey:kHGSExtensionAccountIdentifier];
+		accountIdentifier_ = [[account identifier] retain];
+		if (accountIdentifier_) {
+			// Get bookmarks now, and schedule a timer to update it every hour.
+			[self startAsynchronousBookmarkFetch];
+			[self setUpPeriodicRefresh];
+			// Watch for credential changes.
+			NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+			[nc addObserver:self
+				   selector:@selector(loginCredentialsChanged:)
+					   name:kHGSDidChangeAccountNotification
+					 object:nil];
+		} else {
+			HGSLogDebug(@"Missing account identifier for DeliciousBookmarksSource '%@'",
+						[self identifier]);
+			[self release];
+			self = nil;
+		}
+		 */
+	}
+	return self;
+}
+
+- (void)dealloc {
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+	[deliciousClient_ release];
+	if ([updateTimer_ isValid]) {
+		[updateTimer_ invalidate];
+	}
+	[updateTimer_ release];
+	[accountIdentifier_ release];
+	[tagIcon_ release];
+	
+	[super dealloc];
+}
+
+#pragma mark -
+#pragma mark Bookmarks Fetching
+
+- (void)startAsynchronousBookmarkFetch {
+	if (!deliciousClient_) {
+		/*
+		KeychainItem* keychainItem 
+		= [KeychainItem keychainItemForService:accountIdentifier_
+									  username:nil];
+		if (!keychainItem ||
+			[[keychainItem username] length] == 0 ||
+			[[keychainItem password] length] == 0) {
+			// Can't do much without a login; invalidate so we stop trying (until
+			// we get a notification that the credentials have changed) and bail.
+			[updateTimer_ invalidate];
+			return;
+		}
+		deliciousClient_ = [[DeliciousClient alloc] initWithUsername:[keychainItem username]
+															password:[keychainItem password]];
+		 */
+		deliciousClient_ = [[DeliciousClient alloc] initWithUsername:kHardcodedDeliciousUsername
+															password:kHardcodedDeliciousPassword];
+	}
+	
+	// If we are still marked as fetching, something must have gone wrong with
+	// the last update.  Try to clean it up
+	if (currentlyFetching_) {
+		[deliciousClient_ cancelUpdate];
+	}
+	
+	// Mark us as in the middle of a fetch so that if credentials change during
+	// a fetch we don't destroy the service out from under ourselves.
+	currentlyFetching_ = YES;
+	[deliciousClient_ returnUpdatedDataTo:self
+					handleNewDataSelector:@selector(bookmarkUpdateProducedNewBookmarks:forUser:)
+				   handleNoChangeSelector:@selector(bookmarkUpdateProducedNoChange)
+					handleFailureSelector:@selector(bookmarkUpdateFailedWithError:)];
+}
+
+- (void)setUpPeriodicRefresh {
+	// if we are already running the scheduled check, we are done.
+	if ([updateTimer_ isValid])
+		return;
+	[updateTimer_ release];
+	updateTimer_ = [[NSTimer scheduledTimerWithTimeInterval:(60 * 60)
+													 target:self
+												   selector:@selector(refreshBookmarks:)
+												   userInfo:nil
+													repeats:YES] retain];
+}
+
+- (void)refreshBookmarks:(NSTimer *)timer {
+	[self startAsynchronousBookmarkFetch];
+}
+
+- (void)loginCredentialsChanged:(id)object {
+	/*
+	if ([accountIdentifier_ isEqualToString:object]) {
+		// Make sure we aren't in the middle of waiting for results; if we are, try
+		// again later instead of changing things in the middle of the fetch.
+		if (currentlyFetching_) {
+			[self performSelector:@selector(loginCredentialsChanged:)
+					   withObject:nil
+					   afterDelay:60.0];
+			return;
+		}
+		
+		// Clear the client so that we make a new one with the correct credentials.
+		[deliciousClient_ release];
+		deliciousClient_ = nil;
+
+		// If the login changes, we should update immediately, and make sure the
+		// periodic refresh is enabled (it would have been shut down if the previous
+		// credentials were incorrect).
+		[self startAsynchronousBookmarkFetch];
+		[self setUpPeriodicRefresh];
+	}
+	 */
+}
+
+#pragma mark -
+#pragma mark Bookmark indexing
+
+- (void)bookmarkUpdateFailedWithError:(NSError *)error {
+	currentlyFetching_ = NO;
+	if ([error code] == 403) {
+		// If the login credentials are bad, don't keep trying.
+		[updateTimer_ invalidate];
+	}
+}
+
+- (void)bookmarkUpdateProducedNoChange {
+	currentlyFetching_ = NO;
+}
+
+- (void)bookmarkUpdateProducedNewBookmarks:(NSArray *)bookmarks forUser:(NSString *)username {
+	currentlyFetching_ = NO;
+
+	NSMutableSet *allTags = [NSMutableSet setWithCapacity:50];
+	
+	for (DeliciousBookmark *bookmark in bookmarks) {
+		NSString *url = [bookmark url];
+		NSString *title = [bookmark name];
+		NSSet *tags = [bookmark tags];
+		NSArray *tagArray = [tags allObjects];
+		
+		[allTags addObjectsFromArray:tagArray];
+		
+		HGSObject *result =
+			[self makeResultForUrl: url
+							 title: title
+							  type: HGS_SUBTYPE(kHGSTypeWebBookmark, @"deliciousbookmarks")
+							  tags: tags
+							  icon:nil];
+		
+		[self indexResult:result
+			   nameString:title
+		otherStringsArray:tagArray];
+	}
+	
+	for (NSString *tag in allTags) {
+		NSString *url = [NSString stringWithFormat:@"http://delicious.com/%@/%@", username, tag];
+		HGSObject *result =
+			[self makeResultForUrl:url
+							 title:tag
+							  type:HGS_SUBTYPE(kHGSTypeWebpage, @"delicioustag")
+							  tags: nil
+							  icon:tagIcon_];
+		[self indexResult:result nameString:tag otherString:nil];
+	}
+}
+
+- (HGSObject *) makeResultForUrl: (NSString *)url
+						   title:(NSString *)title
+							type:(NSString *)type
+							tags:(NSSet *)tags
+							icon:(NSImage*)iconImage {
+	NSNumber *rankFlags = [NSNumber numberWithUnsignedInt:eHGSUnderHomeRankFlag];
+	NSMutableDictionary *attributes = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+									   rankFlags, kHGSObjectAttributeRankFlagsKey,
+									   url, kHGSObjectAttributeSourceURLKey,
+									   nil];
+	if (tags) {
+		[attributes setObject:tags forKey:kObjectAttributeDeliciousTags];
+	}
+	
+	if (iconImage) {
+		[attributes setObject:iconImage forKey:kHGSObjectAttributeIconKey];
+	}
+
+	HGSObject* result 
+		= [HGSObject objectWithIdentifier:[NSURL URLWithString:url]
+									 name:([title length] > 0 ? title : url)
+									 type:type
+								   source:self
+							   attributes:attributes];
+	return result;
+}
+
+- (void)processMatchingResults:(NSMutableArray*)results
+                      forQuery:(HGSQuery *)query {
+	// Pivot on tags, filter to only bookmarks with that tag
+	HGSObject *pivotObject = [query pivotObject];
+	if (pivotObject) {
+		NSString *tag = [pivotObject displayName];
+		NSMutableIndexSet *indexesToRemove = [NSMutableIndexSet indexSet];
+		
+		NSUInteger resultCount = [results count];
+		for (NSUInteger idx = 0; idx < resultCount ; ++idx) {
+			HGSObject *result = [results objectAtIndex:idx];
+			NSSet *tags = [result valueForKey:kObjectAttributeDeliciousTags];
+			if (tags && [tags containsObject:tag]) {
+				continue;
+			}
+			
+			[indexesToRemove addIndex:idx];
+		}
+		
+		// remove the indexes that weren't matches
+		[results removeObjectsAtIndexes:indexesToRemove];
+	}	
+}
+
+#pragma mark -
+#pragma mark HGSAccountClientProtocol Methods
+
+- (BOOL)accountWillBeRemoved:(id<HGSAccount>)account {
+	BOOL removeMe = NO;
+	/*
+	NSString *accountIdentifier = [account identifier];
+	if ([accountIdentifier_ isEqualToString:accountIdentifier]) {
+		if (currentlyFetching_) {
+			[deliciousClient_ cancelUpdate];
+		}
+		[deliciousClient_ release];
+		deliciousClient_ = nil;
+		removeMe = YES;
+	}
+	 */
+	return removeMe;
+}
+
+@end
+
---
+++ Modules/DeliciousBookmarks/DeliciousClient.m
@@ -0,0 +1,232 @@
+//
+//  DeliciousClient.m
+//
+//  Copyright (c) 2009 Nathan Parry. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//    * Redistributions of source code must retain the above copyright
+//  notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above
+//  copyright notice, this list of conditions and the following disclaimer
+//  in the documentation and/or other materials provided with the
+//  distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "DeliciousClient.h"
+
+static NSString *const kLastUpdateURL 
+	= @"https://api.del.icio.us/v1/posts/update";
+
+static NSString *const kAllBookmarksURL 
+	= @"https://api.del.icio.us/v1/posts/all";
+
+static NSString *const kPluginUserAgent
+	= @"GoogleQuickSearchBoxDeliciousPlugin/0.1";
+
+@interface DeliciousClient (PrivateMethods)
+- (void)runUpdateCycle;
+- (NSString *)parseForLastUpdateDate:(NSData *)data;
+- (NSArray *)parseForBookmarks:(NSData *)data;
+- (void)openConnectionTo:(NSString *)urlString;
+@end
+
+@implementation DeliciousBookmark
+
+-(id)initWithName:(NSString *)aName
+			  url:(NSString *)aUrl
+			 tags:(NSArray *)theTags {
+	if ((self = [super init])) {
+		name = [aName retain];
+		url =  [aUrl retain];
+		tags = [[NSSet setWithArray:theTags] retain];
+	}
+	return self;
+}
+
+- (void)dealloc {
+	[name release];
+	[url release];
+	[tags release];
+	
+	[super dealloc];
+}
+
+@synthesize name;
+@synthesize url;
+@synthesize tags;
+
+@end
+
+@implementation DeliciousClient
+- (id)initWithUsername:(NSString *)username password:(NSString *) password {
+	if ((self = [super init])) {
+		username_ = [username retain];
+		password_ = [password retain];
+		lastUpdate_ = nil;
+		connection_ = nil;
+		dataBuffer_ = nil;
+	}
+	return self;
+}
+
+- (void)dealloc {
+	[username_ release];
+	[password_ release];
+	[lastUpdate_ release];
+	[connection_ release];
+	[dataBuffer_ release];
+	
+	[super dealloc];
+}
+
+- (void) returnUpdatedDataTo:(id)delegate
+	   handleNewDataSelector:(SEL)newDataSelector
+	  handleNoChangeSelector:(SEL)noChangeSelector
+	   handleFailureSelector:(SEL)failureSelector {
+	delegate_ = delegate;
+	newDataSelector_ = newDataSelector;
+	noChangeSelector_ = noChangeSelector;
+	failureSelector_ = failureSelector;
+	
+	[self runUpdateCycle];
+}
+
+- (void) cancelUpdate {
+	if (connection_) {
+		[connection_ cancel];
+		[connection_ release];
+		connection_ = nil;
+	}
+}
+
+@end
+
+@implementation DeliciousClient (PrivateMethods)
+
+#pragma mark Bookmark update lifecycle
+
+- (void)runUpdateCycle {
+	connectionCompleteCallbackSelector_ = @selector(handleLastUpdateRsp:);
+	[self openConnectionTo:kLastUpdateURL];
+}
+
+- (void)handleLastUpdateRsp:(NSData *)data {	
+	NSString *newLastUpdate = [self parseForLastUpdateDate:data];
+	BOOL upToDate = lastUpdate_ && [lastUpdate_ isEqualToString:newLastUpdate];
+	[lastUpdate_ release];
+	lastUpdate_ = [newLastUpdate retain];
+	
+	if (upToDate) {
+		[delegate_ performSelector:noChangeSelector_];
+	}
+	else {
+		connectionCompleteCallbackSelector_ = @selector(handleBookmarkFetchRsp:);
+		[self openConnectionTo:kAllBookmarksURL];
+	}
+}
+
+- (void)handleBookmarkFetchRsp:(NSData *)data {
+	NSArray *bookmarks = [self parseForBookmarks:data];
+	[delegate_ performSelector:newDataSelector_ withObject:bookmarks withObject:username_];
+}
+
+#pragma mark -
+#pragma mark Delicious response parsing
+
+- (NSString *)parseForLastUpdateDate:(NSData *)data {
+	NSXMLDocument* bookmarksXML =
+		[[[NSXMLDocument alloc] initWithData:data
+									 options:0
+									   error:nil] autorelease];
+	NSArray *updateNodes = [bookmarksXML nodesForXPath:@"//update" error:NULL];
+	return [[(NSXMLElement*)[updateNodes objectAtIndex:0] attributeForName:@"time"] stringValue];
+	
+}
+
+- (NSArray *)parseForBookmarks:(NSData *)data {
+	NSXMLDocument* bookmarksXML =
+		[[[NSXMLDocument alloc] initWithData:data
+									 options:0
+									   error:nil] autorelease];
+	NSArray *bookmarkNodes = [bookmarksXML nodesForXPath:@"//post" error:NULL];
+	NSMutableArray *bookmarks = [[[NSMutableArray alloc] init] autorelease];
+	NSXMLElement *bookmarkNode = nil;
+	for (bookmarkNode in bookmarkNodes) {
+		NSString *name = [[bookmarkNode attributeForName: @"description"] stringValue];
+		NSString *url =  [[bookmarkNode attributeForName: @"href"] stringValue];
+		NSArray *tags = [[[bookmarkNode attributeForName:@"tag"] stringValue]
+				 componentsSeparatedByString:@" "];		
+		DeliciousBookmark *bookmark = [[[DeliciousBookmark alloc]
+										initWithName:name url:url tags:tags] autorelease];
+		[bookmarks addObject: bookmark];
+	}
+	
+	return bookmarks;
+}
+
+#pragma mark -
+#pragma mark Http processing
+
+- (void)openConnectionTo:(NSString *)urlString {
+	[connection_ release];
+	
+	NSURL *url = [NSURL URLWithString:urlString];
+	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+	[request setValue: kPluginUserAgent forHTTPHeaderField:@"User-Agent"];
+	
+	connection_ = [[NSURLConnection connectionWithRequest:request delegate:self] retain];
+}
+
+- (void)connection:(NSURLConnection *)connection 
+didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
+	
+	id<NSURLAuthenticationChallengeSender> sender = [challenge sender];
+	if ([challenge previousFailureCount] < 3) {
+		NSURLCredential *creds 
+		= [NSURLCredential credentialWithUser:username_
+									 password:password_
+								  persistence:NSURLCredentialPersistenceForSession];
+		[sender useCredential:creds forAuthenticationChallenge:challenge];
+	} else {
+		[sender continueWithoutCredentialForAuthenticationChallenge:challenge];
+	}
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+	[dataBuffer_ release];
+	dataBuffer_ = [[NSMutableData alloc] init];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+	[dataBuffer_ appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+	[self performSelector:connectionCompleteCallbackSelector_ withObject:dataBuffer_];
+	[dataBuffer_ release];
+	dataBuffer_ = nil;
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+	[delegate_ performSelector:failureSelector_ withObject:error];
+	[dataBuffer_ release];
+	dataBuffer_ = nil;
+}
+
+#pragma mark -
+@end
