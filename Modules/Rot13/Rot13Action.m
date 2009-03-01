d3d05eec2b64236e7b78184d16658577207f9543
Nathan Parry <nparry@gmail.com>
2009-02-28 20:29:48 -0500

    Force the ROT13 plugin to use a smaller font size for nicer display

--- Modules/Rot13/Rot13Action.m
+++ Modules/Rot13/Rot13Action.m
@@ -1,5 +1,9 @@
 #import <Vermilion/Vermilion.h>
+#import <QuartzCore/QuartzCore.h>
 #import "GTMLargeTypeWindow.h"
+
+static const CGFloat kTwoThirdsAlpha = 0.66;
+static const int kFontSize = 16;
 
 @interface Rot13Action : HGSAction
 @end
@@ -24,8 +28,39 @@
 		NSString *rot13ified = [NSString stringWithCharacters:buffer length:length];
 		free(buffer);
 		
+		// Copy this from the easy large type window initWithString so we can control
+		// the font size.
+		NSMutableAttributedString *attrString
+			= [[[NSMutableAttributedString alloc] initWithString:rot13ified] autorelease];
+		
+		NSRange fullRange = NSMakeRange(0, [rot13ified length]);
+		[attrString addAttribute:NSForegroundColorAttributeName 
+						   value:[NSColor whiteColor] 
+						   range:fullRange];
+		
+		NSMutableParagraphStyle *style 
+			= [[[NSParagraphStyle defaultParagraphStyle] mutableCopy] autorelease];
+		[style setAlignment:NSCenterTextAlignment];
+		[attrString addAttribute:NSParagraphStyleAttributeName 
+						   value:style 
+						   range:fullRange];
+		
+		NSShadow *textShadow = [[[NSShadow alloc] init] autorelease];
+		[textShadow setShadowOffset:NSMakeSize( 5, -5 )];
+		[textShadow setShadowBlurRadius:10];
+		[textShadow setShadowColor:[NSColor colorWithCalibratedWhite:0 
+															   alpha:kTwoThirdsAlpha]];
+		[attrString addAttribute:NSShadowAttributeName 
+						   value:textShadow 
+						   range:fullRange];
+		
+		NSFont *font = [NSFont boldSystemFontOfSize:kFontSize] ;
+		[attrString addAttribute:NSFontAttributeName 
+						   value:font
+						   range:fullRange];
+		
 		GTMLargeTypeWindow *largeTypeWindow
-		= [[GTMLargeTypeWindow alloc] initWithString:rot13ified];
+		= [[GTMLargeTypeWindow alloc] initWithAttributedString:attrString];
 		[largeTypeWindow setReleasedWhenClosed:YES];
 		[largeTypeWindow makeKeyAndOrderFront:self];
 		success = YES;
