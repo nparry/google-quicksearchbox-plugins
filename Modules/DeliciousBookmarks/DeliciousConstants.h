d06f1349d53b59fd02e43cb3430536b814865e33
Nathan Parry <nparry@gmail.com>
2010-01-13 21:29:10 -0500

    Moved plugins to official 3rd party plugin site for QSB

--- Modules/DeliciousBookmarks/DeliciousConstants.h
+++
@@ -1,5 +0,0 @@
-
-#define kLastUpdateURL @"https://api.del.icio.us/v1/posts/update"
-#define kAllBookmarksURL @"https://api.del.icio.us/v1/posts/all"
-#define kPluginUserAgent @"GoogleQuickSearchBoxDeliciousPlugin/0.4"
-#define kObjectAttributeDeliciousTags @"ObjectAttributeDeliciousTags"
